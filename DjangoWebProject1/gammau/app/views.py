c0e2c3bd94fb51cbee31c0b871510508d7659166
NSH531 <NSH531@GMAIL.COM>
2023-06-28 14:03:49 +0300

    add auto-class generator

--- DjangoWebProject1/gammau/app/views.py
+++ DjangoWebProject1/gammau/app/views.py
@@ -85,8 +85,47 @@ def execute_script(request):
                     "persist": True,
                     "session_id": SESSION_A
                 })
+                print(parse_json(EXE2))
         
     return render(request, 'app/execute_script.html')
+
+def parse_json(DATA):
+    # Implement your JSON parsing logic here
+    # Return the parsed data as a dictionary with class names as keys and corresponding attributes as values
+    parsed_data = DATA
+
+    # Create classes dynamically based on parsed data
+    class_dict = {}
+    for class_name, attributes in parsed_data.items():
+        new_class_attrs = {}
+        if   type(attributes)==str:
+             
+             new_class_attrs["attr"] = convert_value(attributes)
+
+        else:
+            for attr_name, attr_value in attributes.items():
+                new_class_attrs[attr_name] = convert_value(attr_value)
+
+        # Create a new class with the class name and attributes
+        new_class = type(class_name, (object,), new_class_attrs)
+        class_dict[class_name] = new_class
+
+    return class_dict
+
+def convert_value(value):
+    # Implement conversion logic as per your requirements
+    # This function converts the value to the appropriate Python type
+    # You can extend this function to handle more data types if needed
+    if isinstance(value, str):
+        return str(value)
+    elif isinstance(value, int):
+        return int(value)
+    elif isinstance(value, float):
+        return float(value)
+    elif isinstance(value, bool):
+        return bool(value)
+    else:
+        return value
 
 def generate_access_token(api_key, api_secret):
     import falconpy
@@ -101,6 +140,10 @@ def generate_access_token(api_key, api_secret):
 
 def main(request):
     assert isinstance(request, HttpRequest)
+    print(parse_json( render(
+        request,
+        'app/main.html'
+    ).content))
     return render(
         request,
         'app/main.html'
