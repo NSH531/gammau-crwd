97d62aac6bcb88b70d452a4808f90237463b054d
NSH531 <NSH531@GMAIL.COM>
2023-07-03 10:00:59 +0300

    as

--- gammau/gammau/settings.py
+++ gammau/gammau/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '63cd6354-b79e-4182-84bb-bac9af217a86'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 # Application references
 # https://docs.djangoproject.com/en/2.1/ref/settings/#std:setting-INSTALLED_APPS
